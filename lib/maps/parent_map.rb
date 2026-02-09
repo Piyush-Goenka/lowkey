612ce5bd7a107fc4dd502d0b03faace2a625155f
maedi <maediprichard@gmail.com>
2026-02-09 21:13:34 +1100

    Rename parent mapper to parent map

--- lib/maps/parent_map.rb
+++ lib/maps/parent_map.rb
@@ -1,22 +1,28 @@
 # frozen_string_literal: true
 
 module Lowkey
-  class ParentMapper < Prism::Visitor
-    attr_reader :parent_map
-
-    def initialize
+  class ParentMap
+    def initialize(root_node:)
       @parent_map = {}
       @current_parent = nil
+
+      map(root_node)
     end
 
-    def visit(node)
+    def parent(node:)
+      @parent_map[node]
+    end
+
+    private
+
+    def map(node)
       @parent_map[node] = @current_parent
 
       old_parent = @current_parent
       @current_parent = node
 
       node.compact_child_nodes.each do |n|
-        visit(n)
+        map(n)
       end
 
       @current_parent = old_parent
