ecb1a406627d839aeed36b90fb16b72346d082fc
maedi <maediprichard@gmail.com>
2026-02-09 21:16:10 +1100

    Test parsing a file

--- spec/units/lowkey_spec.rb
+++ spec/units/lowkey_spec.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../../lib/lowkey'
+require_relative '../../lib/proxies/file_proxy'
 
 RSpec.describe Lowkey do
-  describe '#initialize' do
-    it 'instantiates' do
+  let(:file_path) { 'spec/fixtures/a.rb' }
+
+  describe '.parse' do
+    let(:file_proxy) { Lowkey.parse(file_path:) }
+
+    it 'parses file' do
+      expect(file_proxy).to be_an_instance_of(Lowkey::FileProxy)
     end
   end
 end
