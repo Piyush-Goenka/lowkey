edeaae1de75c402074e3ef5d802e8f853bfa668b
maedi <maediprichard@gmail.com>
2026-02-17 18:39:15 +1100

    Namespace visitors

--- lib/visitors/method_call_visitor.rb
+++ lib/visitors/method_call_visitor.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
-class MethodCallVisitor
-  attr_reader :parent_map
+module Lowkey
+  class MethodCallVisitor
+    attr_reader :parent_map
 
-  def initialize(file_proxy:, parent_map:)
-    @file_proxy = file_proxy
-    @parent_map = parent_map
-  end
+    def initialize(file_proxy:, parent_map:)
+      @file_proxy = file_proxy
+      @parent_map = parent_map
+    end
 
-  def visit(node)
-    class_proxy = @file_proxy.class_proxy(node:, parent_map:)
-    class_proxy.method_calls << node
+    def visit(node)
+      class_proxy = @file_proxy.class_proxy(node:, parent_map:)
+      class_proxy.method_calls << node
 
-    return unless node.name == :private && node.respond_to?(:start_line) && class_proxy.start_line && class_proxy.end_line
-    return unless node.start_line > class_proxy.start_line && node.start_line < class_proxy.end_line
+      return unless node.name == :private && node.respond_to?(:start_line) && class_proxy.start_line && class_proxy.end_line
+      return unless node.start_line > class_proxy.start_line && node.start_line < class_proxy.end_line
 
-    class_proxy.private_start_line = node.start_line
+      class_proxy.private_start_line = node.start_line
+    end
   end
 end
--- lib/visitors/method_class_visitor.rb
+++ lib/visitors/method_class_visitor.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-class MethodClassVisitor
-  attr_reader :parent_map
+module Lowkey
+  class MethodClassVisitor
+    attr_reader :parent_map
 
-  def initialize(file_proxy:, parent_map:)
-    @file_proxy = file_proxy
-    @parent_map = parent_map
-  end
+    def initialize(file_proxy:, parent_map:)
+      @file_proxy = file_proxy
+      @parent_map = parent_map
+    end
 
-  def visit(node)
-    @file_proxy.class_proxy(node:, parent_map:)
+    def visit(node)
+      @file_proxy.class_proxy(node:, parent_map:)
+    end
   end
 end
