be4444a4f08b742efed3880c43a952048ea960c2
obsidiannnn <satya.chauhan1976@gmail.com>
2026-04-04 03:27:18 +0530

    Add binding attr_accessor to ClassProxy for class load phase population

--- lib/proxies/class_proxy.rb
+++ lib/proxies/class_proxy.rb
@@ -4,12 +4,13 @@ require_relative 'module_proxy'
 
 module Lowkey
   class ClassProxy < ModuleProxy
-    attr_accessor :instance_methods
+    attr_accessor :instance_methods, :binding
 
     def initialize(node:, name:, namespace:, source:)
       super(node:, name:, namespace:, source:)
 
       @instance_methods = {}
+      @binding = nil
     end
   end
 end
