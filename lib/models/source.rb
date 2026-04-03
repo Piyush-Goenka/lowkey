24a7ed095fc8118baa69d6f8180974a86580761d
maedi <maediprichard@gmail.com>
2026-04-03 18:38:52 +1100

    Mutate and export lines specific to the source

--- lib/models/source.rb
+++ lib/models/source.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 module Lowkey
+  class WriteError < StandardError; end
+
   class Source
-    attr_reader :file_path, :scope
-    attr_accessor :lines, :start_line, :end_line
+    attr_reader :file_path, :scope, :lines
+    attr_accessor :start_line, :end_line
 
     def initialize(file_path:, scope:, lines:, start_line:, end_line: nil)
       @file_path = file_path
@@ -14,16 +16,26 @@ module Lowkey
       @lines = lines
     end
 
+    def lines=(new_lines)
+      raise WriteError, "More new lines than old lines, won't fit" if new_lines.count > lines.count
+
+      indent = lines[start_index][/^\s+/]
+
+      [start_index..end_index].each do |line_index|
+        lines[line_index] = indent + new_lines.pop.to_s || ''
+      end
+    end
+
     def wrap(prefix:, suffix:)
       start_line = lines[start_index]
       indent = start_line[/^\s+/]
-      lines[start_index] = indent + prefix.to_s + start_line.lstrip
 
+      lines[start_index] = indent + prefix.to_s + start_line.lstrip
       lines[end_index] = lines[end_index] + suffix.to_s
     end
 
     def export
-      lines.join
+      lines[start_index..end_index].join
     end
 
     private
