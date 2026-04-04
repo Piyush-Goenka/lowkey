6d19edd6b64e64ad201b7d4c3ffde4219c3cba2f
obsidiannnn <satya.chauhan1976@gmail.com>
2026-04-04 09:25:23 +0530

    Rename binding to class_binding to avoid confusion with Ruby's binding method

--- lib/proxies/class_proxy.rb
+++ lib/proxies/class_proxy.rb
@@ -4,13 +4,13 @@ require_relative 'module_proxy'
 
 module Lowkey
   class ClassProxy < ModuleProxy
-    attr_accessor :instance_methods, :binding
+    attr_accessor :instance_methods, :class_binding
 
     def initialize(node:, name:, namespace:, source:)
       super(node:, name:, namespace:, source:)
 
       @instance_methods = {}
-      @binding = nil
+      @class_binding = nil
     end
   end
 end
